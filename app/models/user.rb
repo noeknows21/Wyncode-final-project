a360b2ca07a2e56ce8dc4cb940eab437120624ce
Noe <noeon270@hotmail.com>
2016-02-25 13:26:31 -0500

    modify form, add validations, and more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  
+  has_secure_password
+  validates :name, :email, presence: true, length: {maximum: 75}
+  validates :email, uniqueness: { case_sensitive: false },
+            format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, length: { minimum: 6 }
 end
