b1e47fd691d1704204b65bd0f77375000d065e82
Sean Gilchrist <smgilchrist@gmail.com>
2016-02-29 19:09:14 -0500

    added an average of the smile and negative emotions, displaying on the view page.  needs to be refactored

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,13 +1,11 @@
 class EmotionsController < ApplicationController
-  
-  def display
-    
-      headers = {  
-       "app_id" => '4985f625',  
-       "app_key" => '24ad28c4dd3c94df26b2ac78d96a5ccf'  
-      }  
 
-     @response = HTTParty.post('https://api.kairos.com/media?source=https://s3.amazonaws.com/pitchusers/45508312/fa4aa2be-d1c1-436e-b445-fd095c40d744/archive.mp4&timeout=30', { headers: headers })  
+  def display
+    headers = {
+      "app_id" => '4985f625',
+      "app_key" => '24ad28c4dd3c94df26b2ac78d96a5ccf'
+    }
+
+    @response = HTTParty.post('https://api.kairos.com/media?source=https://s3.amazonaws.com/pitchusers/45508312/2bac4a6c-9b78-4bdc-badf-b2a6a521df67/archive.mp4', { headers: headers })
   end
-  
 end
