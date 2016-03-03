875565e12500dc0517ef41d7b9f9aada9141510d
Noe <noeon270@hotmail.com>
2016-03-03 17:57:00 -0500

    fix join sesh text sizing and remove creaton ability from cusstomers

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -46,10 +46,10 @@ class RoomsController < ApplicationController
     end
   end
   
-  private
   
   def check_code
     redirect_to ('https://immense-savannah-32539.herokuapp.com/rooms/create_private/' + params[:code])
+    # redirect_to ('http://localhost:3000/rooms/create_private/' + params[:code])
   end
   
   
