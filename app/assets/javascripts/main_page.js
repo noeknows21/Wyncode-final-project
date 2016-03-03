51951c7139c74e66bfab9d701d05390f006f6922
Tommy Bisi <tommybisi@gmail.com>
2016-03-03 17:05:46 -0500

    main button styling

--- app/assets/javascripts/main_page.js
+++ app/assets/javascripts/main_page.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 
-  $('.demo-btn').focus(function(){         // Changing the clickon border color from PLAY DEMO Button.
+  $('.demo-btn').focus(function() {         // Changing the clickon border color from PLAY DEMO Button.
     $(this).css('outline-color', '#ccc');
   });
 
 
+    $('.demo-btn').mouseenter(function() {
+      $(this).text('EMOTRAK');
+  });
+
+  $('.demo-btn').mouseleave(function() {
+    $(this).text('VIEW APP');
+});
+  // $('.demo-btn').hover(function() {
+  //   $(this).replaceWith('VIEW APP');
+  // });
 
 
-
-
+  // $('arrow-down, i').scroll(function() {
+  //   $(this).fadeOut('fast');
+  // });
 
 
 
