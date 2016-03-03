0a559b1fc8d8ac8d9858d1f000389bb27b298155
Tommy Bisi <tommybisi@gmail.com>
2016-03-03 10:32:09 -0500

    PLAY DEMO btn styling with JS

---
+++ app/assets/javascripts/main_page.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $('.demo-btn').focus(function(){         // Changing the clickon border color from PLAY DEMO Button.
+    $(this).css('outline-color', '#ccc');
+  });
+
+
+
+
+
+
+
+
+
+
+});
