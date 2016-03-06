9ef3534fd1289c89ab124f0e872489ad858a8380
Sean Gilchrist <smgilchrist@gmail.com>
2016-03-05 23:30:11 -0500

    testing heroku

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -48,9 +48,9 @@ class EmotionsController < ApplicationController
   end
 
   def index
-    # @file_path = params[:url]
-    # @file_url = @file_path.split('?').first
-    file_url = "https://s3.amazonaws.com/pitchusers/45508312/0803f85d-c175-4db0-9c35-2c92fc1c8319/archive.zip"
+    file_path = params[:url]
+    file_url = file_path.split('?').first
+    # file_url = "https://s3.amazonaws.com/pitchusers/45508312/0803f85d-c175-4db0-9c35-2c92fc1c8319/archive.zip"
     UnzipTokWorker.perform_async(file_url)
   end
 end
