183d5c51ca365159f81f554f731fe1df4878ebe8
Noe <noeon270@hotmail.com>
2016-03-06 16:35:05 -0500

    interpolation for vid url

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -6,17 +6,27 @@ class EmotionsController < ApplicationController
       "app_id" => '4985f625',
       "app_key" => '24ad28c4dd3c94df26b2ac78d96a5ccf'
     }
+    
+    # @video_url= "https://s3.amazonaws.com/pitchusers/transcoder/output/f29b202b-1de6-4d12-9cf5-b4b37f4e8d7d-1457292784695.mp4"
+    # @formatted_url = "https://api.kairos.com/media?source=#{@video_url}&timeout=59"
+    # @response = HTTParty.post("#{@formatted_url}", { headers: headers })
+    @shit = "https://s3.amazonaws.com/pitchusers/transcoder/output/f29b202b-1de6-4d12-9cf5-b4b37f4e8d7d-1457292784695.mp4"
+    @response = HTTParty.post("https://api.kairos.com/media?source=#{@shit}&timeout=30", { headers: headers })
+    
+    # @response = HTTParty.post("https://api.kairos.com/media?source=https://s3.amazonaws.com/pitchusers/transcoder/output/f29b202b-1de6-4d12-9cf5-b4b37f4e8d7d-1457292784695.mp4&timeout=30", { headers: headers })
 
-    @response = HTTParty.post('https://api.kairos.com/media?source=https://s3.amazonaws.com/pitchusers/transcoder/output/6584065f-4bbd-468e-a52b-2ac94232d1f3-1457221322743.mp4&timeout=30', { headers: headers })
-
-    body = JSON.parse(@response.body)
+    # @body = JSON.parse(@response.body)
+    
+    
+    
+    
     @smile_pre_array = []
     @frown_pre_array = []
     @smile_array = []
     @frown_array = []
-
-
-    body['frames'].each do |f|
+    
+    
+    @response['frames'].each do |f|
       f['person']['emotions'].each do |k,v|
         case k
         when "smile"
@@ -26,16 +36,24 @@ class EmotionsController < ApplicationController
         end
       end
     end
-
-    @smile_pre_array.delete(0)
-    @frown_pre_array.delete(0)
-
-    @smile_pre_array.each_slice(4) {|ele|
-      @smile_array << (ele.inject(:+) / ele.size.to_f)
+    
+    
+    
+    
+    # @smile_pre_array.delete(0)
+    # @frown_pre_array.delete(0)
+    
+    
+    
+    
+    
+    
+    @smile_pre_array.each_slice(30) {|ele|
+      @smile_array << (ele.inject(:+) / ele.size.to_f).round(3)
     }
-
-    @frown_pre_array.each_slice(4) {|ele|
-      @frown_array << (ele.inject(:+) / ele.size.to_f)
+    
+    @frown_pre_array.each_slice(30) {|ele|
+      @frown_array << (ele.inject(:+) / ele.size.to_f).round(3)
     }
 
   end
