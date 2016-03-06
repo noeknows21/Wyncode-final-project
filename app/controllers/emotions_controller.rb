dafe8b0772d9920515c03f6604a893ab52767f7d
Sean Gilchrist <smgilchrist@gmail.com>
2016-03-06 13:15:46 -0500

    p params to test output

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -50,6 +50,8 @@ class EmotionsController < ApplicationController
 
   def index
     file_path = params[:url]
+    p "TESTTESTESTSETESTESTESTESTESTESTESTETSTSETEST"
+    p params
     file_url = file_path.split('?').first
     # file_url = "https://s3.amazonaws.com/pitchusers/45508312/0803f85d-c175-4db0-9c35-2c92fc1c8319/archive.zip"
     UnzipTokWorker.perform_async(file_url)
