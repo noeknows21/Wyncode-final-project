7293470992936a2bbefcf05c1e959589cedc73e4
Noe <noeon270@hotmail.com>
2016-03-05 22:41:38 -0500

    try naming archive

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -34,7 +34,7 @@ class RoomsController < ApplicationController
       @token = chosen_user.token
       
       HTTParty.post('https://api.opentok.com/v2/partner/45508312/archive',
-      { :body => { "sessionId" => "#{@session_id}", "hasAudio" => true, "hasVideo" => true, "outputMode" => "individual"}.to_json,
+      { :body => { "sessionId" => "#{@session_id}", "hasAudio" => true, "hasVideo" => true, "name" => "#{current_user.name}", "outputMode" => "individual"}.to_json,
       :headers => { 'Content-Type' => 'application/json', 'X-TB-PARTNER-AUTH' => '45508312:8face9e9bb645dd756bd1def75b786bbad83ea75'}})  
 
   end
