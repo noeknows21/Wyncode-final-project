e908b94757a1fd04268a05762b3b4f173abb650b
Noe <noeon270@hotmail.com>
2016-03-07 16:40:55 -0500

    logout mistake fix attempt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   #The helper_method line below allows us to use @current_user in our view files
   helper_method :current_user
