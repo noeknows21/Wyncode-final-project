14cd8b4bd89cac315a528aa31317170cf05a1537
Noe <noeon270@hotmail.com>
2016-03-07 20:10:02 -0500

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get '/emotions/index'
   post '/emotions/index'
   get '/info/about_tech'
-  get '/emotions/graph/:url'
+  get '/emotions/graph/:url' => 'emotions#graph'
   get '/emotions/mp4_created'
   post '/emotions/mp4_created'
 
