40c2deb4e065492e90c83faa7e63d6b67c959d20
Noe <noeon270@hotmail.com>
2016-03-07 20:28:55 -0500

    debug video url abomination

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -10,37 +10,32 @@ class EmotionsController < ApplicationController
 
     @video_url = "https://s3.amazonaws.com/pitchusers/transcoder/output/" +params[:url]
     @response = HTTParty.post("https://api.kairos.com/media?source=#{@video_url}&timeout=30", { headers: headers })
-
-    # @response = HTTParty.post("https://api.kairos.com/media?source=https://s3.amazonaws.com/pitchusers/transcoder/output/f29b202b-1de6-4d12-9cf5-b4b37f4e8d7d-1457292784695.mp4&timeout=30", { headers: headers })
-
-    # @body = JSON.parse(@response.body)
-
-    @smile_pre_array = []
-    @frown_pre_array = []
-    @smile_array = []
-    @frown_array = []
-
-    @response['frames'].each do |f|
-      f['person']['emotions'].each do |k,v|
-        case k
-        when "smile"
-          @smile_pre_array << v
-        when "negative"
-          @frown_pre_array << v
-        end
-      end
-    end
-
-    # @smile_pre_array.delete(0)
-    # @frown_pre_array.delete(0)
-
-    @smile_pre_array.each_slice(30) {|ele|
-      @smile_array << (ele.inject(:+) / ele.size.to_f).round(3)
-    }
-
-    @frown_pre_array.each_slice(30) {|ele|
-      @frown_array << (ele.inject(:+) / ele.size.to_f).round(3)
-    }
+    # 
+    # 
+    # @smile_pre_array = []
+    # @frown_pre_array = []
+    # @smile_array = []
+    # @frown_array = []
+    # 
+    # @response['frames'].each do |f|
+    #   f['person']['emotions'].each do |k,v|
+    #     case k
+    #     when "smile"
+    #       @smile_pre_array << v
+    #     when "negative"
+    #       @frown_pre_array << v
+    #     end
+    #   end
+    # end
+    # 
+    # 
+    # @smile_pre_array.each_slice(30) {|ele|
+    #   @smile_array << (ele.inject(:+) / ele.size.to_f).round(3)
+    # }
+    # 
+    # @frown_pre_array.each_slice(30) {|ele|
+    #   @frown_array << (ele.inject(:+) / ele.size.to_f).round(3)
+    # }
 
   end
 
