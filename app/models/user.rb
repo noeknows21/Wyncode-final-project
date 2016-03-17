2a6e22f2cbbce80753775708d079f1ff7fbc7f01
Noe <noeon270@hotmail.com>
2016-03-17 17:22:44 -0400

    reinstate validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :videos, dependent: :destroy
   has_secure_password
-  # validates :name, :email, presence: true, length: {maximum: 75}
-  # validates :email, uniqueness: { case_sensitive: false },
-  #           format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  # validates :password, length: { minimum: 6 }
+  validates :name, :email, presence: true, length: {maximum: 75}
+  validates :email, uniqueness: { case_sensitive: false },
+            format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, length: { minimum: 6 }
   validate :repcode, :repcode_validate
   
   def repcode_validate
